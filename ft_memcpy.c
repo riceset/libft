bad254ae0f79d6bd675f213850cfffe6b5dbc5bf
komeno <tkomeno@student.42tokyo.jp>
2022-04-10 16:42:40 -0300

    changed memcpy and memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 05:43:01 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/04/07 07:41:47 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/04/10 16:36:57 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	
-	if (dst == NULL && src == NULL)
-		return NULL;
+
 	i = 0;
-	while (i < n)
+	if (dst != NULL || src != NULL)
 	{
-		((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
-		i++;
+		while (i < n)
+		{
+			((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+			i++;
+		}
 	}
 	return (dst);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 12:26:24 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/04/08 00:51:48 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/04/10 16:41:09 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	
-	i = 0;
+
 	if (dst < src)
 	{
+		i = 0;
 		while (i < n)
 		{
 			((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
 			i++;
 		}
-		return (dst);
 	}
 	if (dst > src)
-		while (n > 0)
+	{
+		i = n;
+		while (i > 0)
 		{
-			((unsigned char *) dst)[n - 1] = ((unsigned char *) src)[n - 1];
-			n--;
+			((unsigned char *) dst)[i - 1] = ((unsigned char *) src)[i - 1];
+			i--;
 		}
+	}
 	return (dst);
 }
