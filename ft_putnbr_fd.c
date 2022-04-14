8b74097688f540b5076b5f3d6a1b768f7dbbc29d
komeno <tkomeno@student.42tokyo.jp>
2022-04-14 14:02:23 -0300

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,72 +6,30 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 15:29:22 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/04/14 12:00:14 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/04/14 13:49:24 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	get_digits(int n)
-{
-	int	digits;
-
-	digits = 0;
-	while (n != 0)
-	{
-		digits++;
-		n /= 10;
-	}
-	return (digits);
-}
-
-void	prnt(int num, int digits, char numbers[], int fd)
-{
-	int	i;
-
-	i = 0;
-	if (num == 0)
-		ft_putchar_fd('0', fd);
-	else if (num < 0)
-	{
-		ft_putchar_fd('-', fd);
-		while (i < digits)
-		{
-			ft_putchar_fd(numbers[i], fd);
-			i++;
-		}
-	}
-	else
-	{
-		while (i < digits)
-		{
-			ft_putchar_fd(numbers[i], fd);
-			i++;
-		}
-	}
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	int		i;
-	int		tmp;
-	int		digits;
-	char	numbers[10];
-
-	i = 0;
-	tmp = n;
-	digits = get_digits(n);
-	if (tmp == -2147483648)
-		tmp += 1;
-	if (tmp < 0)
-		tmp *= -1;
-	while (i < digits)
+	if (n == INT_MIN)
 	{
-		numbers[digits - i - 1] = (tmp % 10) + '0';
-		tmp /= 10;
-		i++;
+		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
-	if (n == -2147483648)
-		numbers[9] = '8';
-	prnt(n, digits, numbers, fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+	return ;
 }
