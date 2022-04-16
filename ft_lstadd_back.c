16c09098728daeab04b3b417d3599c0bba2c2271
komeno <tkomeno@student.42tokyo.jp>
2022-04-15 22:02:31 -0300

    ✅ lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/15 10:41:16 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/04/15 21:33:30 by tkomeno          ###   ########.fr       */
+/*   Created: 2022/04/15 21:31:41 by tkomeno           #+#    #+#             */
+/*   Updated: 2022/04/15 21:59:56 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (new)
+	if (lst && new)
 	{
-		new->next = *lst;
-		*lst = new;
+		if (!*lst)
+			*lst = new;
+		else
+			ft_lstlast(*lst)->next = new;
 	}
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 04:20:12 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/04/14 17:39:18 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/04/15 21:31:05 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
+/* char	**ft_split(char const *s, char c); */
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
